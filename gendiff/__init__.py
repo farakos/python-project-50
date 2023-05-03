3d73bc9776d853c11625ed3bf0c30d1b613d8c44
Konstantin Faraktinov <frktnv@protonmail.com>
2023-05-03 13:26:50 +0300

    Fix tests

--- gendiff/__init__.py
+++ gendiff/__init__.py
@@ -1,8 +1,10 @@
 from gendiff.data_parser import parse_data
 from gendiff.constants import MARGIN, MARGIN_MULTIPLIER, SIGN, BOOLS
+from gendiff.gendiff import generate_diff
 
 
-__all__ = ('parse_data',
+__all__ = ('generate_diff',
+           'parse_data',
            'MARGIN',
            'MARGIN_MULTIPLIER',
            'SIGN',
